c939ade3ff2b8d796bd6c9d61ba9b6c7849add7a
chinmay <csbhavegithub@gmail.com>
2020-01-19 18:53:38 +0530

    added sqlconnection menudriven program

--- JDBCSQL/Main/src/Main.java
+++ JDBCSQL/Main/src/Main.java
@@ -1,9 +1,27 @@
-
+import java.io.*;
 public class Main {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		DbConnect connect = new DbConnect();
-		connect.ShowData();	
+		System.out.println("\nMenu");
+		System.out.println("\n1.Insert data");
+		System.out.println("\n2.Update data");
+		System.out.println("\n3.Delete data");
+		System.out.println("\n4.Show Table");
+		System.out.println("\nEnter Your Choice: ");
+		int ch = Integer.parseInt(br.readLine());
+		switch(ch){
+		case 1:
+				connect.Insert();
+				break;
+		case 2: connect.Update();
+				break;
+		case 3: connect.Delete();
+				break;		
+		case 4: connect.ShowData();
+				break;
+		default: System.out.println("Please Enter Valid Choice...");
+		}
 	}
 
 }
