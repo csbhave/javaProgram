14623b61b2546017a6382f2e24a124ea5e90e660
chinmay <csbhavegithub@gmail.com>
2020-01-21 09:46:19 +0530

    added servlet program

---
+++ FirstServlet/src/test/ServletProgram1.java
@@ -0,0 +1,17 @@
+package test;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+@WebServlet("/pqr")
+public class ServletProgram1 extends HttpServlet{
+	public void init(){}
+	public void service(ServletRequest req, ServletResponse res)throws IOException, ServletException
+	{
+		PrintWriter pw= res.getWriter();
+		res.setContentType("text/html");
+		pw.println("Welcome to servlet programming");
+		
+	}
+	public void destroy(){}
+}
